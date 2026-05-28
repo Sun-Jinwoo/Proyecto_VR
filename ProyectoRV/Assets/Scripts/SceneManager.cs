6d5203ff09ec1718109c9ebf9da3165897cc9123
SebMen22 <sebastian.menesesm@uao.edu.co>
2026-05-28 14:56:25 -0500

    MENUVBR
    AAAAAAAA

---
+++ ProyectoRV/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuVR : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
